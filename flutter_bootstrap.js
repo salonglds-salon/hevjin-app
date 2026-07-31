2378a0d590f9eab9123a601bcf61472aaaa11be3
Kasim <dalshkas@amazon.com>
2026-07-31 14:17:40 +0200

    Add block/unblock users feature in chat + filter from discover

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "651576824" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3221827853" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
