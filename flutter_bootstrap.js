e52e7673895c4197e4ec1c653323521552185d53
Kasim <dalshkas@amazon.com>
2026-08-01 13:26:00 +0200

    Fix build - revert broken profile localization, keep working translations

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4209307451" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1162406161" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
