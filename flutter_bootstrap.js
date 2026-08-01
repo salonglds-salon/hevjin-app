3d4924f32c4785dac1b833195c2dc2b465cef21c
Kasim <dalshkas@amazon.com>
2026-08-01 12:37:46 +0200

    Add Farsi (Iran) language

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "967096660" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3738994468" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
