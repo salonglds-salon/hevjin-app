5e3990251e4bb29e7bd8d16f3aa9ec368ccbc34b
Kasim <dalshkas@amazon.com>
2026-08-01 09:46:35 +0200

    Auto-refresh chat list every 10 seconds - smooth preview update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "607249885" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2036549795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
