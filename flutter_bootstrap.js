01c9ae9a0e6cd947730baa0c7b26d74ca20d767c
Kasim <dalshkas@amazon.com>
2026-08-06 14:52:05 +0200

    Fix Welcome Screen: white bg, dark text, proper button colors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1936973222" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2042524176" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
