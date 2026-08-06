82bb0c108a53b92774d338228456de4ec24b1b0c
Kasim <dalshkas@amazon.com>
2026-08-06 15:19:16 +0200

    Remove ALL brown colors from entire app - white/grey/gold/red theme

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1744203707" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3454624807" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
