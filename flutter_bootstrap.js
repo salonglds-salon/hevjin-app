1c040a1b2e63cca4452a2bf4f49028b44be67cb5
Kasim <dalshkas@amazon.com>
2026-08-07 11:53:55 +0200

    Fix chat list flicker: batch queries (N+1 to 3), signature-based change detection, ValueKey on tiles

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1469452932" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3500906948" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
