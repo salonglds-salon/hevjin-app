fd035afb558c4e6fa0448e02fd94d351277d64cf
Kasim <dalshkas@amazon.com>
2026-08-11 15:52:35 +0200

    Datenschutzerklaerung: Art. 9 DSGVO, Auftragsverarbeiter, Konto-Loeschung, Telefonnummer entfernt

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1279374416" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "255533727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
