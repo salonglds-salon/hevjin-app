f0d70296823f9393f833306aba08feee0e56892f
Dalshad Kasim <dalshkas@gmail.com>
2026-09-15 14:16:32 +0200

    Discover: Foto-Ausrichtung auf -0.4 (weniger Hintergrund ueber dem Kopf)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3225133841" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2832250636" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
