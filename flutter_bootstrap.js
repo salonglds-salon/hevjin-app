011f80090148271f69ea83e0b2838093cc05bd05
Dalshad Kasim <dalshkas@gmail.com>
2026-09-15 15:02:58 +0200

    Registrierung: 18+-Gate

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "871349947" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1971459801" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
